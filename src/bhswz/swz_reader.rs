58a5d04289444558b593c12445356550c9b3810a
EyalZusiman <eyal.zusiman@gmail.com>
2025-05-20 22:08:20 +0300

    implement swz reading

--- src/bhswz/swz_reader.rs
+++ src/bhswz/swz_reader.rs
@@ -1,14 +1,36 @@
 use super::SwzRandom;
-use std::io::Read;
+use std::{
+    io::{Read, Write},
+    string::FromUtf8Error,
+};
 
+use flate2::read::ZlibDecoder;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum SwzReaderInitError {
-    #[error("transparent")]
+    #[error(transparent)]
     IOError(#[from] std::io::Error),
     #[error("key checksum mismatch (expected {expected:?}, found {calculated:?})")]
-    InvalidKeyChecksum { expected: u32, calculated: u32 },
+    KeyChecksumMismatch { expected: u32, calculated: u32 },
+}
+
+#[derive(Error, Debug)]
+pub enum SwzReaderReadError {
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error("file checksum mismatch (expected {expected:?}, found {calculated:?})")]
+    FileChecksumMismatch { expected: u32, calculated: u32 },
+    #[error("decompressed file size mismatch (expected {expected:?}, found {calculated:?})")]
+    DecompressedFileSizeMismatch { expected: u32, calculated: u64 },
+}
+
+#[derive(Error, Debug)]
+pub enum SwzReaderReadStringError {
+    #[error(transparent)]
+    ReadError(#[from] SwzReaderReadError),
+    #[error(transparent)]
+    DecodeError(#[from] FromUtf8Error),
 }
 
 pub struct SwzReader<R> {
@@ -31,7 +53,7 @@ where
         let calculated_checksum = calculate_key_checksum(key, &mut random);
 
         if checksum != calculated_checksum {
-            return Err(SwzReaderInitError::InvalidKeyChecksum {
+            return Err(SwzReaderInitError::KeyChecksumMismatch {
                 expected: checksum,
                 calculated: calculated_checksum,
             });
@@ -42,6 +64,48 @@ where
             random: random,
         });
     }
+
+    pub fn read_file<W: Write>(&mut self, writer: &mut W) -> Result<(), SwzReaderReadError> {
+        let mut buf = [0u8; 4];
+        self.reader.read_exact(&mut buf)?;
+        let compressed_size = u32::from_be_bytes(buf) ^ self.random.next();
+        self.reader.read_exact(&mut buf)?;
+        let decompressed_size = u32::from_be_bytes(buf) ^ self.random.next();
+        self.reader.read_exact(&mut buf)?;
+        let checksum = u32::from_be_bytes(buf);
+
+        let mut file_buf = Vec::with_capacity(compressed_size as usize);
+        self.reader
+            .by_ref()
+            .take(compressed_size.into())
+            .read_to_end(&mut file_buf)?;
+
+        let calculated_checksum = decrypt_buffer(&mut file_buf[..], &mut self.random);
+        if checksum != calculated_checksum {
+            return Err(SwzReaderReadError::FileChecksumMismatch {
+                expected: checksum,
+                calculated: calculated_checksum,
+            });
+        }
+
+        let mut zlib = ZlibDecoder::new(&file_buf[..]);
+        let bytes_read = std::io::copy(&mut zlib, writer)?;
+        if bytes_read != decompressed_size.into() {
+            return Err(SwzReaderReadError::DecompressedFileSizeMismatch {
+                expected: decompressed_size,
+                calculated: bytes_read,
+            });
+        }
+
+        return Ok(());
+    }
+
+    pub fn read_file_to_string(&mut self) -> Result<String, SwzReaderReadStringError> {
+        let mut buf = Vec::new();
+        self.read_file(&mut buf)?;
+        let str = String::from_utf8(buf)?;
+        return Ok(str);
+    }
 }
 
 fn calculate_key_checksum(key: u32, random: &mut SwzRandom) -> u32 {
@@ -49,6 +113,16 @@ fn calculate_key_checksum(key: u32, random: &mut SwzRandom) -> u32 {
     let rounds = key % 31 + 5;
     for _ in 0..rounds {
         checksum ^= random.next();
+    }
+    return checksum;
+}
+
+fn decrypt_buffer(buf: &mut [u8], random: &mut SwzRandom) -> u32 {
+    let mut checksum = random.next();
+    for i in 0..buf.len() {
+        let xor = ((random.next() >> (i % 16)) & 0xFF) as u8;
+        buf[i] ^= xor;
+        checksum = checksum.rotate_right((i % 7 + 1) as u32) ^ (buf[i] as u32);
     }
     return checksum;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use std::error::Error;
+use std::fs::{self, File};
 mod bhswz;
-use bhswz::SwzRandom;
+use bhswz::{SwzRandom, SwzReader};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut random = SwzRandom::new(0);
     for _ in 0..100 {
         println!("{}", random.next());
     }
+
+    let file = File::open("C:/Program Files (x86)/Steam/steamapps/common/Brawlhalla/Dynamic.swz")?;
+    let mut reader = SwzReader::new(file, 659849070)?;
+
+    let mut buf = Vec::new();
+    while let Ok(_) = reader.read_file(&mut buf) {
+        let name = random.next().to_string();
+        let path =
+            format!("C:/Program Files (x86)/Steam/steamapps/common/Brawlhalla/Dynamic/{name}.xml");
+        fs::write(path, &buf)?;
+
+        buf.clear();
+    }
+
+    Ok(())
 }
